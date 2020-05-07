6d4ad2290254d3f34571d4ff5efb5bf49f0e9cc2
Kabir Kapur <kapka6700@gmail.com>
2020-05-07 16:13:25 -0700

    initial commit -- pagination yet to be figured out

---
+++ fecapi.py
@@ -0,0 +1,47 @@
+# gqSCsqNyEqmnJzLT6iyhAWcz1vJeBbGycNkm9Gyv -- current key
+# reminder -- 81 elements per dictionary entry
+
+import requests, json
+import pandas as pd
+
+class Fecdata:
+	def __init__(self, min_date = "2019-07-01", committee_id = "C00401224", key = "gqSCsqNyEqmnJzLT6iyhAWcz1vJeBbGycNkm9Gyv"):
+		self.min_date = min_date
+		self.committee_id = committee_id
+		self.key = key
+
+	def create_payload_and_url(self, min_date, committee_id, key):
+		# url with my key -- will change to include .env implementation
+		url = "https://fec-dev-api.app.cloud.gov/v1/schedules/schedule_a/?api_key="
+		self.url_with_key = url + self.key
+		data = requests.get(self.url_with_key + "&committee_id=" + str(self.committee_id))
+		self.payload = {"committee_id" : committee_id, 
+					"per_page": 100, 
+					"min_date" : min_date,
+					"sort" : "contribution_receipt_date",
+					"pages" : data.json()["pagination"]["pages"] # must run endpoint once to find this values -- might work on optimizing this
+					}
+# self.payload["pages"]
+	def make_df(self, payload = self.payload):	#O(n^2) i think
+
+		df = pd.DataFrame()
+		data_list = []
+		for contribution in range(self.payload["pages"]): #iterate through the json file 'pages' number of times	
+			# url_with_key = "https://fec-dev-api.app.cloud.gov/v1/schedules/schedule_a/?api_key=gqSCsqNyEqmnJzLT6iyhAWcz1vJeBbGycNkm9Gyv"
+			paginated_data = requests.get(self.url_with_key, self.payload)
+			# -- iterably append to our dataframe
+			for entry in paginated_data.json()["results"]:
+				data_list.append(entry)
+			self.payload["last_index"] = paginated_data.json()["pagination"]["last_indexes"]["last_index"]
+			self.payload["last_contribution_receipt_date"] = paginated_data.json()["pagination"]["last_indexes"]["last_contribution_receipt_date"]
+		print(self.payload["pages"])
+# items to conduct search through API
+
+# url with my key -- will change to include .env implementation
+
+
+
+req = Fecdata()
+req.create_payload_and_url(req.min_date, req.committee_id, req.key)
+# req.make_df(req.payload)
+req.make_df(req.payload)
