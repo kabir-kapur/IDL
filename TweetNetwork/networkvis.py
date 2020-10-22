29b4cf95052391faa6dc329c43c532a522f7d46b
krkapur@ucsc.edu <kabirkapur@Kabirs-MacBook-Pro.local>
2020-10-21 22:33:08 -0700

    networkx

--- TweetNetwork/networkvis.py
+++ TweetNetwork/networkvis.py
@@ -1,6 +1,12 @@
-import csv, os, plotly
+import os, csv
 import networkx as nx
+import matplotlib.pyplot as plt
 
-G = nx.Graph()
-G.add_node(1)
-print(G)
+G = nx.DiGraph()
+
+G.add_node('Kabir')
+G.add_node('Sex')
+G.add_edge('Kabir', 'Sex')
+
+nx.draw_spectral(G, with_labels = True, node_size = 3000)
+plt.show()
